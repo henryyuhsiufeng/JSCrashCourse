49673111b4cbe57fa835676825effed4b84519b9
Henry Feng <henryfeng12345@gmail.com>
2019-07-25 15:00:34 -0500

    add boilerplate to index and make basic callback function in callback.js

--- callbacks.js
+++ callbacks.js
@@ -1,4 +1,20 @@
+// This example simulates a basic blog and fetching data from the server
+
 const posts = [
     {title: 'Post One', body: 'This is post one'},
     {title: 'Post Two', body: 'This is post two'}
 ];
+
+function getPosts() {
+    // takes in a callback function. Function will be called after some time.
+    setTimeout(() => {
+        let output = '';
+        // Can also pass in an index if you want
+        posts.forEach((post) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+getPosts();

7e9872af43f6642060915de7972d0ce7e2bbd7d8
Henry Feng <henryfeng12345@gmail.com>
2019-07-25 15:10:06 -0500

    complete basic demo of callbacks

--- callbacks.js
+++ callbacks.js
@@ -17,4 +17,19 @@ function getPosts() {
     }, 1000);
 }
 
+function createPost(post, callback) {
+    setTimeout(() => {
+        posts.push(post);
+        callback()
+    }, 2000);
+}
+
 getPosts();
+// WITHOUT CALLBACK 
+    // because createPost takes longer, the method will not make a change
+    // Once the dom has been painted. It can't be changed. 
+    // createPost({title:'Post Three', body: 'This is post three'});
+
+//WITH CALLBACK
+    //createPost will push the third object before getPosts outputs it in index
+createPost({title:'Post Three', body: 'This is post three'}, getPosts);
